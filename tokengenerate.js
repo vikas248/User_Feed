6511c26656fba60e0ba8e285dcd60156755f5c2e
Vikas Shukla <shuklavikas379@gmail.com>
2023-07-28 13:44:57 +0530

    Update tokengenerate.js

--- tokengenerate.js
+++ tokengenerate.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+import express from 'express'
+import jwt from 'jsonwebtoken' 
 const app = express();
 const port = 4000;
 
 const user = {
-    id: "1",
-    name: "ayush",
-    role: "Super Admin",
     email: "ayush@gmail.com",
     password: "ayush@123"
   };
@@ -15,7 +11,7 @@ const user = {
 app.use(express.json());
 
 function generateToken(user) {
-    return jwt.sign({ id: user.id, name:user.name, role:user.role, email: user.email, password:user.password }, 'your-secret-key', {
+    return jwt.sign({email: user.email, password:user.password }, 'your-secret-key', {
       expiresIn: '1h', // Token expires in 1 hour
     });
   }
