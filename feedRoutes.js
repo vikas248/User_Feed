df3e56cad7779bbb976ff274cda658cb38c5e77c
Vikas Shukla <shuklavikas379@gmail.com>
2023-07-28 17:19:59 +0530

    Update feedRoutes.js

--- feedRoutes.js
+++ feedRoutes.js
@@ -13,17 +13,18 @@ feedRouter.post('/', authenticateJWT, async (req, res) => {
     }
   
     const { title, content } = req.body;
-  
-    try {
-      const connection = await pool.getConnection();
-      const [result] = await connection.query('INSERT INTO Feed (title, content) VALUES (?, ?)', [title, content]);
-      connection.release();
-  
-      res.status(201).json({ id: result.insertId, title, content });
-    } catch (error) {
-      console.error('Error creating feed:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+
+    const query = ('INSERT INTO Feed (title, content) VALUES (?, ?)', [title, content]);
+
+  // Execute the query
+    pool.query(query, (err, rows) => {
+      if (err) {
+        console.error('Error creating feeds:', err);
+        res.status(500).json({ message: 'Error creating feeds.' });
+      } else {
+        res.json(rows);
+      }
+    });
 });
 
 // Read all feeds (accessible to Super Admin)
@@ -33,17 +34,18 @@ feedRouter.get('/', authenticateJWT, async (req, res) => {
     if (role !== 'Admin' && role !== 'Super Admin') {
       return res.status(403).json({ error: 'Access forbidden. Only Admin and Super Admin can read all feeds.' });
     }
-  
-    try {
-      const connection = await pool.getConnection();
-      const [rows] = await connection.query('SELECT id, title, content FROM Feed');
-      connection.release();
-  
-      res.json(rows);
-    } catch (error) {
-      console.error('Error fetching feeds:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+
+    const query = ('SELECT id, title, content FROM Feed');
+
+  // Execute the query
+    pool.query(query, (err, rows) => {
+      if (err) {
+        console.error('Error fetching feeds:', err);
+        res.status(500).json({ message: 'Error fetching feeds.' });
+      } else {
+        res.json(rows);
+      }
+    });
 });
 
 // Read a single feed by ID (accessible to Super Admin and Admins with access)
@@ -55,21 +57,18 @@ feedRouter.get('/:id', authenticateJWT, async (req, res) => {
     }
   
     const { id } = req.params;
-  
-    try {
-      const connection = await pool.getConnection();
-      const [rows] = await connection.query('SELECT id, title, content FROM Feed WHERE id = ?', [id]);
-      connection.release();
-  
-      if (rows.length === 0) {
-        return res.status(404).json({ error: 'Feed not found' });
+
+    const query = ('SELECT id, title, content FROM Feed WHERE id = ?', [id]);
+
+  // Execute the query
+    pool.query(query, (err, rows) => {
+      if (err) {
+        console.error('Error fetching feeds by id', err);
+        res.status(500).json({ message: 'Error fetching feeds.' });
+      } else {
+        res.json(rows);
       }
-  
-      res.json(rows[0]);
-    } catch (error) {
-      console.error('Error fetching feed:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+    });
 });
 
 // Update a feed by ID (accessible to Super Admin only)
@@ -82,21 +81,19 @@ feedRouter.put('/:id', authenticateJWT, async (req, res) => {
   
     const { id } = req.params;
     const { title, content } = req.body;
-  
-    try {
-      const connection = await pool.getConnection();
-      const [result] = await connection.query('UPDATE Feed SET title = ?, content = ? WHERE id = ?', [title, content, id]);
-      connection.release();
-  
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ error: 'Feed not found' });
+
+    const query = ('UPDATE Feed SET title = ?, content = ? WHERE id = ?', [title, content, id]);
+
+  // Execute the query
+    pool.query(query, (err, rows) => {
+      if (err) {
+        console.error('Error updating feeds by id', err);
+        res.status(500).json({ message: 'Error updating feeds.' });
+      } else {
+        res.json(rows);
       }
-  
-      res.json({ message: 'Feed updated successfully' });
-    } catch (error) {
-      console.error('Error updating feed:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+    });
+
 });
 
 // Delete a feed by ID (accessible to Super Admin only)
@@ -109,7 +106,6 @@ feedRouter.delete('/:id', authenticateJWT, async (req, res) => {
   
     const { id } = req.params;
   
-    try {
       const connection = await pool.getConnection();
   
       // Check if the feed exists
@@ -125,7 +121,7 @@ feedRouter.delete('/:id', authenticateJWT, async (req, res) => {
       for (const admin of admins) {
         if (admin.id !== req.user.id) {
           // Super Admin should not be able to delete their own access on a feed
-          const [permissions] = await connection.query('SELECT * FROM Feed_Permissions WHERE feed_id = ? AND user_id = ? AND can_delete = 1', [id, admin.id]);
+          const [permissions] = await connection.execute('SELECT * FROM Feed_Permissions WHERE feed_id = ? AND user_id = ? AND can_delete = 1', [id, admin.id]);
   
           if (permissions.length > 0) {
             return res.status(403).json({ error: 'Access forbidden. Only the assigned admin can delete the feed.' });
@@ -134,18 +130,18 @@ feedRouter.delete('/:id', authenticateJWT, async (req, res) => {
       }
   
       // Delete the feed
-      const [result] = await connection.query('DELETE FROM Feed WHERE id = ?', [id]);
-      connection.release();
-  
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ error: 'Feed not found' });
+
+    const query = ('DELETE FROM Feed WHERE id = ?', [id]);
+
+  // Execute the query
+    pool.query(query, (err, rows) => {
+      if (err) {
+        console.error('Error deleting feeds by id', err);
+        res.status(500).json({ message: 'Error deleting feeds.' });
+      } else {
+        res.json(rows);
       }
-  
-      res.json({ message: 'Feed deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting feed:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+    });
+  });
 
 export default feedRouter;
